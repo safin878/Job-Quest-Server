0b7c356cf52ff5f9e827cc0b800871df00ed799b
safin878 <shafin6747@gmail.com>
2024-05-12 10:08:59 +0600

    Job Related Api Make

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://jobquest-c5a28.web.app",
+    "https://jobquest-c5a28.firebaseapp.com",
+  ],
   credentials: true,
   optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
+app.use(cookieParser());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c9pict7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,8 +35,31 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //Collection
+    const jobsCollection = client.db("JobQuest").collection("AddJobs");
+
+    //Job Add Api
+    app.post("/AddJobs", async (req, res) => {
+      const AddJobData = req.body;
+      const result = await jobsCollection.insertOne(AddJobData);
+      res.send(result);
+    });
+
+    // Job Get Api
+    app.get("/AddJobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
+
+    //  Job Get Api by single id
+
+    app.get("/AddJobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -38,7 +67,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
