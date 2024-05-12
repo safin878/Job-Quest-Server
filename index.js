f14ac8de715f7ffb5cb41f3399e2b8f0e2dab65c
safin878 <shafin6747@gmail.com>
2024-05-12 21:32:44 +0600

    Upadate Job Api Setup

--- index.js
+++ index.js
@@ -75,6 +75,30 @@ async function run() {
       res.send(result);
     });
 
+    // Job Delete Api by single id
+    app.delete("/MyJobId/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //update jod
+    app.put("/MyJobId/:id", async (req, res) => {
+      const id = req.params.id;
+      const jobData = req.body;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...jobData,
+        },
+      };
+      const result = await jobsCollection.updateOne(query, updateDoc, options);
+
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
